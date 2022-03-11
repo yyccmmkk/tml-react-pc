3de953879b05975bf0dfa803f60f278d995d8e3c
yyccmmkk <36995800@163.com>
2022-03-11 23:35:42 +0800

    antd 主题配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.less';
 
 import { Button } from 'antd';
 
@@ -8,20 +8,6 @@ function App() {
   return (
     <div className="App">
       <Button type="primary">Button</Button>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
