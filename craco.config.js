7331b4050a5116fbb129aacaada2369d6d68d0c3
yyccmmkk <36995800@163.com>
2022-03-11 23:43:58 +0800

    antd 主题配置预留

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,6 @@
 const CracoLessPlugin = require('craco-less');
+const {getThemeVariables} = require('antd/dist/theme');
+
 
 module.exports = {
   plugins: [
@@ -7,7 +9,12 @@ module.exports = {
       options: {
         lessLoaderOptions: {
           lessOptions: {
+            /*modifyVars: getThemeVariables({
+              dark: true, // 开启暗黑模式
+              compact: true, // 开启紧凑模式
+            }),*/
             modifyVars: {
+
               /*'@primary-color': '#1DA57A'*/
               // '@primary-color: #1890ff; // 全局主色
               // '@link-color: #1890ff; // 链接色
