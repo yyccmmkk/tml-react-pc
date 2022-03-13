c9ac0b8092b11a365c0f4bddd38c0de03c08fd52
yyccmmkk <36995800@163.com>
2022-03-13 16:59:18 +0800

    antd 配置中文

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.less';
+import {ConfigProvider, Button, DatePicker } from 'antd';
 
-import { Button } from 'antd';
+// 由于 antd 组件的默认文案是英文，所以需要修改为中文
+import zhCN from 'antd/lib/locale/zh_CN';
+import moment from 'moment';
+import 'moment/locale/zh-cn';
+
+moment.locale('zh-cn');
+
 
 function App() {
   return (
-    <div className="App">
-      <Button type="primary">Button</Button>
-    </div>
+    <ConfigProvider locale={zhCN}>
+      <div className="App">
+        <DatePicker />
+        <Button type="primary">Button</Button>
+      </div>
+    </ConfigProvider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {store} from "./redux";
 import { Provider } from 'react-redux';
+import './index.css';
 
 
 
