c3054905d5f6a11a8db7bf011a401d76f95c5ea4
zhoulongfei <36995800@163.com>
2024-03-14 17:57:32 +0800

    feat:生产打包脚本优化

--- pro.ps1
+++ pro.ps1
@@ -1,4 +1,6 @@
+npm run build
 rm * -Include *.tar.gz, *.zip
 tar -cvzf ./build.tar.gz ./build
-7z.exe a  -r ./build.zip ./build
+#7z.exe a  -r ./build.zip ./build
+Compress-Archive ./build ./build.zip
 write-host 'zip压缩完成!' -ForegroundColor green
